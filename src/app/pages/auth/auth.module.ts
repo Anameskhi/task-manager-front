0bfab793660c126f00e51236bec25eb691458bee
Anameskhi <ameskhi@unisens.ge>
2023-02-01 22:22:20 +0400

    added authentication material

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,18 +1,17 @@
+import { RouterModule } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
-import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { MatButtonModule} from '@angular/material/button';
-import { MatInputModule} from '@angular/material/input';
-import { MatIconModule} from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule} from '@angular/material/card'
-import { MatToolbarModule} from '@angular/material/toolbar'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatTabsModule} from '@angular/material/tabs';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,16 +22,17 @@ import { MatToolbarModule} from '@angular/material/toolbar'
   imports: [
     CommonModule,
     AuthRoutingModule,
-    CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatInputModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    FlexLayoutModule,
     MatCardModule,
-    MatToolbarModule
+    FlexLayoutModule,
+    MatTabsModule,
+    MatInputModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule
     
+    
+   
   ]
 })
 export class AuthModule { }

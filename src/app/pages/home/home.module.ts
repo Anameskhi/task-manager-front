a601b7565111c29ffe9f038466353b3e1e63abaf
luca <lucajoglidze@gmail.com>
2023-02-01 13:57:01 +0400

    board page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { BoardComponent } from '../board/board.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [HomeComponent, BoardComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}

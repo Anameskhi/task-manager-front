b6dd0de3bc674aa0cb976a614586cabae23f6f31
luca <lucajoglidze@gmail.com>
2023-02-02 14:13:43 +0400

    add pages

---
+++ src/app/pages/home/constructin/constructin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-constructin',
+  templateUrl: './constructin.component.html',
+  styleUrls: ['./constructin.component.scss']
+})
+export class ConstructinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/home/side-board/side-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-board',
+  templateUrl: './side-board.component.html',
+  styleUrls: ['./side-board.component.scss']
+})
+export class SideBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

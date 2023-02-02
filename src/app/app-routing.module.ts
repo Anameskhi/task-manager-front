22c413f8c21e9798cdfbdf46b40b1ef01ee79f47
luca <lucajoglidze@gmail.com>
2023-02-02 16:18:36 +0400

    add register and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { BoardComponent } from './pages/board/board.component';
 
 const routes: Routes = [
   {
-<<<<<<< HEAD
+
     path: '',
     redirectTo: 'auth',
     pathMatch: 'full'
-=======
+  },{
+
+  
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
->>>>>>> a0dbe270cd06723615378e59cc7f5dec2fb894e0
+
   },
   {
     path: '',
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -9,10 +9,11 @@ import { HomeComponent } from './home.component';
 import { HomeStartComponent } from './home-start/home-start.component';
 import { ConstructinComponent } from './constructin/constructin.component';
 import { SideBoardComponent } from './side-board/side-board.component';
+import { HeaderComponent } from '../header/header.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, HomeStartComponent, ConstructinComponent, SideBoardComponent],
+  declarations: [HomeComponent, HomeStartComponent, ConstructinComponent, SideBoardComponent, HeaderComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,

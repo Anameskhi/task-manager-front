f819c879c3365dcbc8a0d83dafb09ca33d5291e6
lasha giorgadze <lasha.giorgadze12@gmail.com>
2023-02-12 17:01:38 +0400

    brd markup

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -12,38 +12,10 @@ import { AddTaskComponent } from './modals/add-task/add-task.component';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
-  todo?:any = [];
-
-  done?:any= [];
-  animal?: string;
   
-  // constructor(public dialog: MatDialog) {console.log(this.todo); }
-  // openDialog(): void {
-    
-  //   const dialogRef = this.dialog.open(AddTaskComponent, 
-  //     {
-  //     data: {todos: this.todo, dones: this.done},
-  //     }
-    
-  //   );
-        
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     console.log('The dialog was closed' + result);
-      
-  //   });
-  // }
 
   ngOnInit() {
   }
 
-  // drop(event: CdkDragDrop<string[]>) {
-  //   if (event.previousContainer === event.container) {
-  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  //   } else {
-  //     transferArrayItem(event.previousContainer.data,
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex);
-  //   }
-  // }
+
 }

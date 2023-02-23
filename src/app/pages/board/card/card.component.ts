6e574e3e9b5dabf991b32992643b3802a0dc624d
lasha giorgadze <lasha.giorgadze12@gmail.com>
2023-02-23 19:37:10 +0400

    draging columns addition

--- src/app/pages/board/card/card.component.ts
+++ src/app/pages/board/card/card.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 import { ListComponent } from '../list/list.component';
 
@@ -14,4 +15,22 @@ export class CardComponent {
     var newList = new ListComponent();
     this.lists.push(newList);
   }
+
+  onColumnDrop(event: CdkDragDrop<any>) {
+    moveItemInArray(this.lists, event.previousIndex, event.currentIndex);
+  }
+  // drop(event: CdkDragDrop<any>) {
+  //   if (event.previousContainer === event.container) {
+  //     moveItemInArray(
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex);
+  //   } else {
+  //     transferArrayItem(
+  //       event.previousContainer.data,
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex);
+  //   }
+  // }
 }

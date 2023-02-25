7e3344f2b53aa5b9c9e2535eb7d0dac969ba8aa9
lasha giorgadze <lasha.giorgadze12@gmail.com>
2023-02-25 14:25:02 +0400

    router add

--- src/app/pages/home/board-form/board-form.component.ts
+++ src/app/pages/home/board-form/board-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Route, Router, RouterLink } from '@angular/router';
 import { BoardFormService } from 'src/app/core/services/board-form.service';
 
 @Component({
@@ -13,7 +14,9 @@ export class BoardFormComponent implements OnInit {
   color = '';
   background =
     'https://plus.unsplash.com/premium_photo-1674752365557-166d7edc8081?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80';
-  constructor(private boardFormSrv: BoardFormService) {}
+  constructor(
+    private boardFormSrv: BoardFormService,
+    private router: Router) {}
   ngOnInit(): void {
     this.backgroundColor = this.boardFormSrv.boardColors;
     this.backgroundImg = this.boardFormSrv.boardImages;
@@ -25,5 +28,7 @@ export class BoardFormComponent implements OnInit {
   }
   onSubmit(form: NgForm) {
     console.log(form);
+    this.router.navigate(['/board'])
+    
   }
 }
